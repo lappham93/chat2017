343e51ed95d1935a04e149116e243d8029eeecf2
Lap <lappham93@gmail.com>
2017-07-31 18:51:35 +0700

    register

---
+++ chat-admin/src/main/java/com/mit/bodies/RegisterBody.java
@@ -0,0 +1,19 @@
+package com.mit.bodies;
+
+public class RegisterBody {
+	private String userName;
+	private String password;
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
--- chat-admin/src/main/java/com/mit/controllers/RegisterController.java
+++ chat-admin/src/main/java/com/mit/controllers/RegisterController.java
@@ -2,17 +2,31 @@ package com.mit.controllers;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.math.NumberUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.mit.app.enums.AppType;
+import com.mit.bodies.RegisterBody;
+import com.mit.http.ApiResponse;
+import com.mit.sessions.AdminSession;
 import com.mit.user.bodies.SocialSignup;
+import com.mit.user.entities.Admin;
+import com.mit.user.entities.AdminProfile;
+import com.mit.user.enums.AdminType;
+import com.mit.user.repositories.AdminProfileRepo;
+import com.mit.user.repositories.AdminRepo;
 import com.mit.user.responses.LoginToken;
+import com.mit.user.responses.RefreshTokenResponse;
 import com.mit.user.services.LoginService;
+import com.mit.utils.AdminConstant;
+import com.mit.utils.AuthenticateUtils;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -27,6 +41,10 @@ public class RegisterController {
 	@Value("${admin.prefix}")
 	private String adminPrefix;
 	
+	@Autowired
+	private AdminProfileRepo adminProfileRepo;
+	@Autowired
+	private AdminRepo adminRepo;
 
 	@ApiOperation(value = " register", notes = "Redirect to register if profile is null")
 	@RequestMapping(method = RequestMethod.POST)
@@ -60,5 +78,34 @@ public class RegisterController {
 		}
 
 		return mv;
+	}
+	
+	public ApiResponse<Object> register(HttpServletRequest request, @RequestBody RegisterBody body) {
+		try {
+			AdminProfile ap = new AdminProfile(AdminType.SYSTEM_ADMIN.getValue());
+			ap.setStatus(1);
+			ap.setFirstName("");
+			ap.setLastName("");
+			adminProfileRepo.save(ap);
+	
+			Admin admin = new Admin();
+			admin.setUserName(body.getUserName());
+			admin.setProfileId(ap.getId());
+			String salt = RandomStringUtils.random(10);
+			String password = body.getPassword();
+			admin.setSalt(salt);
+			admin.setPassword(AuthenticateUtils.hashPassword(password, salt));
+			admin.setStatus(1);
+			adminRepo.save(admin);
+			
+			RefreshTokenResponse token = loginService.generateTokens(ap, AppType.CLIENT.getValue());
+			if (token != null) {
+				AdminSession.setSession(request, token.getSessionKey(), token.getRefreshToken());
+				return new ApiResponse<>(0, AdminConstant.loginSuccessMsg);
+			}
+			return new ApiResponse<>();
+		} catch (Exception e) {
+		}
+		return new ApiResponse<>(AdminConstant.errCode, AdminConstant.serverErrMsg);
 	}
 }
